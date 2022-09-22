cb7cc72dff7a51127fbc52f78e370240e880680d
steve <stevzhang01@gmail.com>
2022-09-22 12:27:47 +0800

    feat: text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          BTFS Storage3 Test Project!
         </a>
       </header>
     </div>
